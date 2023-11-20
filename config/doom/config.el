154879f0c4fce803d0fda8c254a0d411dd004629
Marc Alvarez <marcalvarez@fastmail.com>
2023-11-19 22:29:16 -0700

    Accept our AI overlords into the Church of Emacs

--- config/doom/config.el
+++ config/doom/config.el
@@ -91,3 +91,11 @@
 
 ;; Just a little transparency
 (set-frame-parameter (selected-frame) 'alpha '(90 . 90)) (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
